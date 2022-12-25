f89e7f2ce9176aece6d3749dbff4c88d39b6cb18
duskxi <84715902+duskxi@users.noreply.github.com>
2022-12-25 09:05:51 +0000

    服务器初始化

---
+++ web_init.py
@@ -0,0 +1,15 @@
+from api_server import *
+from env import Environment
+from web_server import ApiServer
+
+components = [Files, Image, FileWords, ImageBase64, Save]
+
+
+def init_web(env: Environment) -> tuple[ApiServer, list[Component]]:
+    api_server = ApiServer()
+    instantiated_components = []
+    for componentClass in components:
+        _component = componentClass(env)
+        instantiated_components.append(_component)
+        api_server.register(_component.url, _component.view, _component.methods)
+    return api_server, instantiated_components
