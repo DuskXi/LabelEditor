b72e31a3f519bb7660cc08df1c3ef4ce005c127a
duskxi <84715902+duskxi@users.noreply.github.com>
2022-12-25 09:05:45 +0000

    修正

--- web_server.py
+++ web_server.py
@@ -1,14 +1,23 @@
+import logging
+
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 
 class ApiServer:
-    def __init__(self, static_url_path='', static_folder="public"):
+    def __init__(self, static_url_path='', static_folder="public/dist/spa"):
         self.app = Flask(__name__,
                          static_url_path='',
                          static_folder=static_folder)
+        self.app.config['JSON_AS_ASCII'] = False
+        cors = CORS(self.app)
+        self.app.config['CORS_HEADERS'] = 'Content-Type'
+        log = logging.getLogger('werkzeug')
+        log.disabled = True
 
     def run(self):
-        self.app.run()
+        self.app.run(threaded=True)
 
-    def register(self, path, func):
-        self.app.add_url_rule(path, view_func=func)
+    def register(self, path, func, methods=['GET']):
+        func.__func__.__name__ += path
+        self.app.add_url_rule(path, view_func=func, methods=methods)
